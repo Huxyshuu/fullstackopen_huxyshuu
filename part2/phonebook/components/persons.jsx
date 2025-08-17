7c3cc916c4d1bde4d6fd57f04ea62c8ef157fc44
Huxyshuu <hugotamm@hotmail.com>
2025-08-17 14:27:27 +0300

    part2d done with asterisk as well ^^

--- part2/phonebook/components/persons.jsx
+++ part2/phonebook/components/persons.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-export default function persons({ searchResults }) {
+export default function persons({ searchResults, deletePerson }) {
   return (
     <div>
         {
         searchResults.map((person, id) => {
-          return <p key={`${person.name}-${id}`}>{person.name} {person.number}</p>
+          return <p key={`${person.name}-${id}`}>{person.name} {person.number} <button onClick={() => deletePerson(person.id, person)}>delete</button></p>
         })
         }
     </div>
--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import SearchBar from '../components/searchBar.jsx'
 import PersonForm from '../components/personForm.jsx'
 import Persons from '../components/persons.jsx'
+import personService from '../services/persons/personService.js'
 import axios from 'axios'
 
 const App = () => {
@@ -26,17 +27,53 @@ const App = () => {
       name: newName,
       number: newNumber,
     } 
-    if (persons.some(e => e.name === newName )) {
-      alert(`${newName} is already added to phonebook`)
+    if (persons.some(p => p.name === newName )) {
+      if (window.confirm(`${newName} is already added to to phonebook, replace the old number with a new one?`)) {
+        const person = persons.find(p => p.name === newName);
+        const changedPerson = { ...person, number: newNumber}
+
+        personService
+        .update(person.id, changedPerson)
+        .then(returnedPerson => {
+          console.log(returnedPerson)
+          setPersons(persons.map(p => p.id === person.id ? returnedPerson.data : p));
+          setSearchResults(searchResults.map(p => p.id === person.id ? returnedPerson .data: p));
+        })
+        .catch(error => {
+          console.error('Error updating person:', error);
+        });
+      }
     } else {
-      setPersons(persons.concat(personToSubmit));
-      setSearchResults(searchResults.concat(personToSubmit));
+      personService
+        .create(personToSubmit)
+        .then(response => {
+          const responsePerson = response.data;
+          setPersons(persons.concat(responsePerson));
+          setSearchResults(searchResults.concat(responsePerson));
+        })
+        .catch(error => {
+          console.error('Error creating person:', error);
+        });
+    }
+  }
+
+  const deletePerson = (id, person) => {
+    if (window.confirm(`Delete ${person.name}?`)) {
+      personService
+      .deletePerson(id)
+      .then(() => {
+        setPersons(persons.filter(p => p.id !== id));
+        setSearchResults(searchResults.filter(p => p.id !== id));
+      })
+      .catch(error => {
+        console.error('Error deleting person:', error);
+      });
     }
   }
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
         setSearchResults(searchResults.concat(response.data));
@@ -50,7 +87,7 @@ const App = () => {
       <h3>add a new</h3>
       <PersonForm handleSubmit={handleSubmit} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
       <h3>Numbers</h3>
-      <Persons searchResults={searchResults}/>
+      <Persons searchResults={searchResults} deletePerson={deletePerson}/>
     </div>
   )
 }
